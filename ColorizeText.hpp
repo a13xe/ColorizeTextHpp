21df1c158185f953861daebd3d8c9b6bf769dec9
a13xe <77492646+a13xe@users.noreply.github.com>
2023-11-22 00:44:12 +0300

    Create ColorizeText.hpp

---
+++ ColorizeText.hpp
@@ -0,0 +1,37 @@
+#ifndef COLORIZE_HPP
+#define COLORIZE_HPP
+#include <iostream>
+#include <string>
+
+
+// ANSI color codes
+enum Color 
+{
+    BLACK = 30,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    WHITE
+};
+
+
+// Print colorized text
+void colorized_print(const std::string& message, Color textColor, Color bgColor, int boldness)
+{
+    std::string colorCode = "\033[";
+
+    if (boldness == 1)
+    {
+        colorCode += "1;";
+    }
+
+    colorCode += std::to_string(textColor) + ";" + std::to_string(bgColor + 10) + "m";
+
+    std::cout << colorCode << message << "\033[0m" << std::endl;
+}
+
+
+#endif // COLORIZE_HPP
